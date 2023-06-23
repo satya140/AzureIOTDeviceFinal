c0b0fddb4cdfd9daaeee7c76bbaebe5349f51933
satya <satya140@gmail.com>
2023-06-23 12:47:06 +0530

    Add project files.

---
+++ Controllers/IOTDeviceController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureIOTDevice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IOTDeviceController : ControllerBase
+    {
+
+        private readonly string _connectionString = "HostName=iothub23rdjune.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=LTK2d+c/RHurWEoEz5L0oPC55yEn1Jsj1/0PGxZ2qO8=";
+       // private readonly string _deviceId = "deviceid-01";
+
+        private readonly ManageDevice manageDevice;
+
+        public IOTDeviceController()
+        {
+            manageDevice = new ManageDevice();
+        }
+        // GET: api/<IOTDeviceController>
+        [HttpGet("GetAllDeviceList")]
+        public async Task<IActionResult> GetAllDeviceList()
+        {
+            return Ok(await manageDevice.GetDeviceListAsync(_connectionString));
+        }
+
+        //// GET api/<IOTDeviceController>/5
+        //[HttpGet("")]
+        //public async Task<IActionResult> Get(string id)
+        //{
+        //    await manageDevice.GetDeviceTwinAsync(_connectionString, id);
+        //}
+
+        // POST api/<IOTDeviceController>
+        [HttpPost("CreateDevice")]
+        public async Task<IActionResult> CreateDevice( string deviceId)
+        {
+           return Ok(await manageDevice.CreateDeviceAsync(_connectionString, deviceId));
+        }
+
+        // PUT api/<IOTDeviceController>/5
+        [HttpPut("UpdatedesiredProperties")]
+        public async Task<IActionResult> UpdatedesiredProperties(string deviceid)
+        {
+            return Ok(await manageDevice.UpdateDesiredPropertiesAsync(_connectionString, deviceid));
+        }
+
+
+        // PUT api/<IOTDeviceController>/5
+        [HttpPut("UpdateReportedProperties")]
+        public async Task<IActionResult> UpdateReportedProperties(string deviceid)
+        {
+            return Ok(await manageDevice.UpdateReportedPropertiesAsync(_connectionString, deviceid));
+        }
+
+        // DELETE api/<IOTDeviceController>/5
+        [HttpDelete("DeleteDevice")]
+        public async Task<IActionResult> DeleteDevice(string deviceid)
+        {
+            return Ok(await manageDevice.DeleteDeviceAsync(_connectionString, deviceid));
+        }
+
+
+        [HttpPost("Sendtelemetry")]
+        public async Task<IActionResult> SendTelemetry(string deviceid)
+        {
+            return Ok(await manageDevice.SendDeviceTocloudMessageAsync(_connectionString, deviceid));
+        }
+    }
+}
---
+++ ManageDevice.cs
@@ -0,0 +1,272 @@
+﻿
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using Microsoft.Azure.Devices.Shared;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureIOTDevice
+{
+    public class ManageDevice
+    {
+        
+        public ManageDevice()
+        {
+            //_configuration = configuration;
+            //_connectionString = "HostName=iothub23rdjune.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=LTK2d+c/RHurWEoEz5L0oPC55yEn1Jsj1/0PGxZ2qO8=";
+
+        }
+
+        public async Task<string> CreateDeviceAsync(string connectionString, string deviceId)
+        {
+            // Create a new instance of the RegistryManager using the IoT Hub connection string
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+
+            try
+            {
+                // Check if the device already exists
+                Device device = await registryManager.GetDeviceAsync(deviceId);
+                if (device != null)
+                {
+                    //Console.WriteLine("Device already exists!");
+                    //Console.WriteLine($"Reading Device twin: ");
+
+                    await GetDeviceTwinAsync(connectionString,deviceId);
+                    return "Device already exists!"; ;
+                }
+
+                // Create a new device instance
+                device = new Device(deviceId);
+
+                // Register the device in the IoT Hub
+                device = await registryManager.AddDeviceAsync(device);
+
+                // Console.WriteLine("Device created successfully!");
+
+                var deviceInfo = new
+                {
+                    Msg = "Device Created",
+                    Id = device.Id,
+                    PrimaryKey = device.Authentication.SymmetricKey.PrimaryKey,
+                    SecondaryKey = device.Authentication.SymmetricKey.SecondaryKey
+                };
+
+                return JsonConvert.SerializeObject(deviceInfo);
+
+                //await GetDeviceTwinAsync(connectionString, deviceId);
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                return "Device already exists!";
+            }
+            catch (Exception ex)
+            {
+                return "Error creating device: {ex.Message}" +ex.Message;
+            }
+            finally
+            {
+                await registryManager.CloseAsync();
+            }
+        }
+
+        public async Task<string> GetDeviceTwinAsync(string connectionString,string deviceId)
+        {
+
+            DeviceClient dClient = DeviceClient.CreateFromConnectionString(connectionString, deviceId);
+
+            try
+            {
+                Twin twin = await dClient.GetTwinAsync();
+                return twin.ToJson();
+                //var list = JsonConvert.DeserializeObject<List<ManageDevice>>(myJsonString);
+                //list.Add(new Person(1234, "carl2");
+                //var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
+
+                //List<string> devicename= new List<string>();
+                //ManageDevice m= new ManageDevice(); 
+                //DesiredProperties d= new DesiredProperties();
+                //d.DeviceName = twin.DeviceId;
+                //d.DesiredProperty.Add(twin.Properties.Desired.ToJson());
+                //d.ReportedProperty.Add(twin.Properties.Reported.ToJson());
+                // devicename.Add(d);
+
+
+                //Console.WriteLine($"Device Twin received. Device ID: {twin.DeviceId}");
+                //Console.WriteLine($"Desired properties: {twin.Properties.Desired.ToJson()}");
+                //Console.WriteLine($"Reported properties: {twin.Properties.Reported.ToJson()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return string.Empty;
+            }
+            finally
+            {
+                await dClient.CloseAsync();
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetDeviceListAsync(string connectionString)
+        {
+
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            List<string> iotdevice = new List<string>();
+            try
+            {
+                var devices = registryManager.CreateQuery("SELECT * FROM devices", 100);
+                
+                while (devices.HasMoreResults)
+                {
+                    var page = await devices.GetNextAsTwinAsync();
+
+                    foreach (var twin in page)
+                    {
+                        //Console.WriteLine($"Device ID: {twin.DeviceId}");
+                        //Console.WriteLine($"Connection state: {twin.ConnectionState}");
+                        //Console.WriteLine($"Last activity: {twin.LastActivityTime}");
+                        //Console.WriteLine("-------------------------------------");
+
+                        //iotdevice.Add(twin.ToJson());
+                        iotdevice.Add(twin.DeviceId);
+                    }
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                //return "Error creating device: {ex.Message}" + ex.Message;
+                //return null;
+            }
+            finally
+            {
+                await registryManager.CloseAsync();
+            }
+            return iotdevice;
+        }
+
+        public async Task<string> DeleteDeviceAsync(string connectionString,string deviceId)
+        {
+            Console.WriteLine($"Deleting device - {deviceId}");
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+
+            try
+            {
+                await registryManager.RemoveDeviceAsync(deviceId);
+                return "Device {deviceId} deleted successfully!";
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Error: {ex.Message}");
+                return ex.ToString();
+            }
+            finally
+            {
+                await registryManager.CloseAsync();
+            }
+        }
+
+        public async Task<string> UpdateDesiredPropertiesAsync(string connectionString,string deviceId)
+        {
+
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            string message = string.Empty;
+            try
+            {
+                Twin twin = await registryManager.GetTwinAsync(deviceId);
+
+
+                // Update desired properties
+                twin.Properties.Desired["temp"] = "20";
+
+                // Update the twin in the IoT Hub
+                await registryManager.UpdateTwinAsync(twin.DeviceId, twin, twin.ETag);
+
+                message = "Desired properties ['temp':20]updated successfully!";
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+            finally
+            {
+                await registryManager.CloseAsync();
+            }
+            return message;
+        }
+
+        public async Task<string> UpdateReportedPropertiesAsync(string connectionString,string deviceId)
+        {
+
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            string message = string.Empty;
+            try
+            {
+                Twin twin = await registryManager.GetTwinAsync(deviceId);
+
+                var reportedProperties = new TwinCollection();
+
+
+                reportedProperties["DeviceLocation"] = new { lat = 32.768, lon = 97.023, alt = 0 };
+
+                DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(connectionString, deviceId, Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+                await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+                return message = "Reported properties updated successfully! " + JsonConvert.SerializeObject(reportedProperties);
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+            finally
+            {
+                await registryManager.CloseAsync();
+            }
+        }
+
+        public async Task<string> SendDeviceTocloudMessageAsync(string connectionString,string deviceId)
+        {
+
+            // RegistryManager registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            DeviceClient dClient = DeviceClient.CreateFromConnectionString(connectionString, deviceId, Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+            {
+                try
+                {
+                    Random rand = new Random();
+                    double minTemp = 15;
+                    double minHumidity = 50;
+
+                    while (true)
+                    {
+                        double currentTemp = minTemp + rand.NextDouble() * 15;
+                        double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                        //Create Json Message
+                        var telemetryDataPoint = new
+                        {
+                            temp = currentTemp,
+                            humidity = currentHumidity
+
+                        };
+                        string messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+
+                        var message = new Microsoft.Azure.Devices.Client.Message(Encoding.ASCII.GetBytes(messageString));
+
+                        await dClient.SendEventAsync(message);
+                        return messageString;// = "{0} > Sending message: {1}" //+ DateTime.Now, messageString;
+                        //await Task.Delay(1000);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return $"Error: {ex.Message}";
+                }
+                finally { await dClient.CloseAsync(); }
+
+            }
+        }
+    }
+}
